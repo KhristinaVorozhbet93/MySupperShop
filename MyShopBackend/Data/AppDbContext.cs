a438663c08e072e1c5e075ca1ad8068d61d9f045
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-18 19:55:26 +0400

    Добавлена таблица Account

--- MyShopBackend/Data/AppDbContext.cs
+++ MyShopBackend/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace MyShopBackend.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Product> Products => Set<Product>();
+        public DbSet<Account> Accounts => Set<Account>();
 
         public AppDbContext(
             DbContextOptions<AppDbContext> options)
--- MySupperShop/Program.cs
+++ MySupperShop/Program.cs
@@ -9,7 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<IMyShopClient>(new MyShopClient("https://localhost:7252"));
